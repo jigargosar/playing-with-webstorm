9a23d4e08610eed0ed88517e6c19e18b2f108fe3
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-25 19:16:59 +0530

    WIP

--- src/lib/react-ext.js
+++ src/lib/react-ext.js
@@ -1,10 +1,9 @@
-import { map } from 'ramda'
 import React from 'react'
 
 export const renderKeyedById = (Component, propName, list) =>
-  map(model =>
+  list.map(model =>
     React.createElement(Component, {
       key: model.id,
       [propName]: model,
     }),
-  )(list)
+  )
