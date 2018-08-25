e8a85c9db9ead21a150325dfaa3adb45bb2fc442
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-25 16:54:50 +0530

    init

--- src/lib/fake.js
+++ src/lib/fake.js
@@ -1,7 +1,10 @@
 import faker from 'faker'
 import {Chance} from 'chance'
 
-export const chance = Chance()
+let seed = Math.random();
+
+export const chance = Chance(seed)
+faker.seed(seed)
 
 export function randomWords() {
   return faker.random.word()
