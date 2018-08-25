16af94e633fa282b7aa801dfed0f56e815e8e005
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-25 10:02:21 +0530

    init

--- src/App.js
+++ src/App.js
@@ -142,16 +142,24 @@ function SidebarContent() {
   );
 }
 
+function HeaderContent() {
+  return <div className={"f2 ma2"}>fixed header</div>;
+}
+
+function FooterContent() {
+  return <div className={"pa2 f4"}>fixed footer</div>;
+}
+
 function App() {
   return (
     <Fragment>
       <div className={"flex flex-column vh-100 overflow-hidden bg-black-30"}>
-        <div className={"bg-black-30"}>fixed header</div>
+        <div className={"bg-black-30"}><HeaderContent/></div>
         <div className={"flex"}>
-          <div className={'w-30 mw6 overflow-scroll '}><SidebarContent /></div>
-          <div className={'flex-auto overflow-scroll bg-black-30'}><MainContent /></div>
+          <div className={'w-30 mw6 overflow-scroll '}><SidebarContent/></div>
+          <div className={'flex-auto overflow-scroll bg-black-30'}><MainContent/></div>
         </div>
-        <div className={'bg-black-30'}>fixed footer</div>
+        <div className={'bg-black-30'}><FooterContent/></div>
       </div>
     </Fragment>
   );
