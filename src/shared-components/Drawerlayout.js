6c0693b894bea671d9124ffdd56cb6b4c2d8acb9
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-25 12:10:44 +0530

    init

--- src/shared-components/Drawerlayout.js
+++ src/shared-components/Drawerlayout.js
@@ -1,8 +1,9 @@
-import React, {Fragment} from 'react'
+import React from 'react'
 import cn from 'classname'
+import PropTypes from 'prop-types'
 
 export function DrawerLayout({
-  styleClass="bg-near-white sans-serif",
+  styleClass = 'bg-near-white sans-serif',
   children,
   header,
   footer,
@@ -10,37 +11,43 @@ export function DrawerLayout({
   debug = false,
 }) {
   return (
-    <Fragment>
-      <div
-        className={cn('flex flex-column vh-100 overflow-hidden ', styleClass, {
-          'bg-black-20': debug,
-        })}
-      >
-        <div className={cn({ 'bg-black-20 ba bw1 b--blue': debug })}>
-          {header}
-        </div>
-        <div className={'flex-auto flex '}>
-          {sidebar && (
-            <div
-              className={cn('w-30 mw6 overflow-scroll', {
-                'bg-black-40 ba bw1 b--pink': debug,
-              })}
-            >
-              {sidebar}
-            </div>
-          )}
+    <div
+      className={cn('flex flex-column vh-100 overflow-hidden ', styleClass, {
+        'bg-black-20': debug,
+      })}
+    >
+      <div className={cn({ 'bg-black-20 ba bw1 b--blue': debug })}>
+        {header}
+      </div>
+      <div className={'flex-auto flex '}>
+        {sidebar && (
           <div
-            className={cn('flex-auto overflow-scroll ', {
-              'bg-black-50 ba bw1 b--green': debug,
+            className={cn('w-30 mw6 overflow-scroll', {
+              'bg-black-40 ba bw1 b--pink': debug,
             })}
           >
-            {children}
+            {sidebar}
           </div>
-        </div>
-        <div className={cn({ 'bg-black-20 ba bw1 b--red': debug })}>
-          {footer}
+        )}
+        <div
+          className={cn('flex-auto overflow-scroll ', {
+            'bg-black-50 ba bw1 b--green': debug,
+          })}
+        >
+          {children}
         </div>
       </div>
-    </Fragment>
-  )
+      <div className={cn({ 'bg-black-20 ba bw1 b--red': debug })}>
+        {footer}
+      </div>
+    </div>  )
+}
+
+DrawerLayout.propTypes = {
+  debug: PropTypes.bool,
+  styleClass: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  header: PropTypes.node,
+  footer: PropTypes.node,
+  sidebar: PropTypes.node,
 }
