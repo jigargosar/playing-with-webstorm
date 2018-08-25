812c6c3c9648489f5068958104d59f716ce77d2a
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-25 17:01:40 +0530

    init

--- src/lib/fake.js
+++ src/lib/fake.js
@@ -1,7 +1,7 @@
 import faker from 'faker'
 import {Chance} from 'chance'
 
-let seed = Math.random();
+let seed = Math.random()
 
 export const chance = Chance(seed)
 faker.seed(seed)
@@ -9,3 +9,46 @@ faker.seed(seed)
 export function randomWords() {
   return faker.random.word()
 }
+
+var jsf = require('json-schema-faker')
+
+var schema = {
+  type: 'object',
+  properties: {
+    user: { $ref: '#/definitions/user' },
+  },
+  required: ['user'],
+  definitions: {
+    user: {
+      type: 'object',
+      properties: {
+        id: {
+          $ref: '#/definitions/positiveInt',
+        },
+        name: {
+          type: 'string',
+          faker: 'name.findName',
+        },
+        email: {
+          type: 'string',
+          format: 'email',
+          faker: 'internet.email',
+        },
+      },
+      required: ['id', 'name', 'email'],
+    },
+    positiveInt: {
+      type: 'integer',
+      minimum: 0,
+      exclusiveMinimum: true,
+    },
+  },
+}
+
+jsf.resolve(schema).then(function(sample) {
+  console.log(sample)
+  // "[object Object]"
+
+  console.log(sample.user.name)
+  // "John Doe"
+})
