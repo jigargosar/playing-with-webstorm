c769750051037f3988cfec3b78781159b6fa6459
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-25 11:42:49 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Fragment} from "react";
 import "tachyons";
+import cn from 'classname'
 
 function RepeatString({ value = "default string", times = 30 }) {
   return (
@@ -16,24 +17,24 @@ function FooterContent() {
   return <div className={"pa2 f4"}>fixed footer</div>;
 }
 
-function DrawerLayout({ children, header, footer, sidebar }) {
+function DrawerLayout({ children, header, footer, sidebar, debug=false }) {
   return (
     <Fragment>
-      <div className={"flex flex-column vh-100 overflow-hidden bg-black-20"}>
-        <div className={"bg-black-20 ba bw1 b--blue"}>{header}</div>
+      <div className={cn("flex flex-column vh-100 overflow-hidden ", {'bg-black-20':debug})}>
+        <div className={cn({"bg-black-20 ba bw1 b--blue":debug})}>{header}</div>
         <div className={"flex-auto flex "}>
           {sidebar && <div
-            className={"w-30 mw6 overflow-scroll bg-black-40 ba bw1 b--pink"}
+            className={cn("w-30 mw6 overflow-scroll",{"bg-black-40 ba bw1 b--pink":debug})}
           >
             {sidebar}
           </div>}
           <div
-            className={"flex-auto overflow-scroll bg-black-50 ba bw1 b--green"}
+            className={cn("flex-auto overflow-scroll ",{"bg-black-50 ba bw1 b--green":debug})}
           >
             {children}
           </div>
         </div>
-        <div className={"bg-black-20 ba bw1 b--red"}>{footer}</div>
+        <div className={cn({"bg-black-20 ba bw1 b--red":debug})}>{footer}</div>
       </div>
     </Fragment>
   );
@@ -42,9 +43,10 @@ function DrawerLayout({ children, header, footer, sidebar }) {
 function App() {
   return (
     <DrawerLayout
-    // header={<HeaderContent />}
-    // footer={<FooterContent />}
-    // sidebar={<RepeatString value={"side bar item"} times={3} />}
+    header={<HeaderContent />}
+    footer={<FooterContent />}
+    sidebar={<RepeatString value={"side bar item"} times={3} />}
+    debug={false}
     >
       <RepeatString value={"main content items"} times={3} />
     </DrawerLayout>
