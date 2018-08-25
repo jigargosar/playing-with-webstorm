d38639b73d295338422e967061126fecdd036c7f
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-25 18:50:41 +0530

    init

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {getTitle, Task} from './models/Task'
 import {times} from 'ramda'
 
 function App() {
+  const tasks = times(() => Task())(30)
   return (
     <MainLayout title={'FunDo'}>
       {times(i => <div key={i}>{getTitle(Task())}</div>)(30)}
