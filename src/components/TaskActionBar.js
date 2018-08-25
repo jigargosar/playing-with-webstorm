4bb08a677c9d33d2da07bb2337e6cb05881a4572
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-25 22:23:09 +0530

    WIP

--- src/components/TaskActionBar.js
+++ src/components/TaskActionBar.js
@@ -1,24 +1,37 @@
-import { map } from 'ramda'
+import { identity, isNil, map } from 'ramda'
 import React, { Fragment } from 'react'
+import { isDone } from '../models/Task'
 
-function renderButton(label) {
+function renderButton(label, handleClick) {
   return (
     <div className="ph2">
-      <button className="ph2">{label}</button>
+      <button onClick={handleClick} className="ph2">
+        {label}
+      </button>
     </div>
   )
 }
 
-export function TaskActionBar() {
+export function TaskActionBar({ queries, actions }) {
+  const task = queries.selectedTask
+
   const buttonProps = [
-    { label: 'add' },
-    { label: 'done' },
-    { label: 'delete' },
+    { label: 'add', handleClick: identity },
+    {
+      label: 'done',
+      handleClick: () => {
+        if (isNil(task)) return
+        actions.setDone(!isDone(task), task)
+      },
+    },
+    { label: 'delete', handleClick: identity },
   ]
   return (
     <div className="pa2 frr bt b--silver">
-      {map(({ label }) => (
-        <Fragment key={label}>{renderButton(label)}</Fragment>
+      {map(({ label, handleClick }) => (
+        <Fragment key={label}>
+          {renderButton(label, handleClick)}
+        </Fragment>
       ))(buttonProps)}
     </div>
   )
