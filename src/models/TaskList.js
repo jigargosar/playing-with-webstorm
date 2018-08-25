075984c17d7793a65f1cfab3c08b85bf4676d281
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-25 22:27:36 +0530

    WIP

--- src/models/TaskList.js
+++ src/models/TaskList.js
@@ -9,10 +9,6 @@ import {
 import { assert } from '../lib/assert'
 
 export const overModel = curry(({ id }, fn, list) => {
-  return overModelId(id, fn, list)
-})
-
-export const overModelId = curry(({ id }, fn, list) => {
   const taskIdx = findIndex(propEq('id', id))(list)
   assert(taskIdx !== -1)
   return over(lensIndex(taskIdx), fn, list)
