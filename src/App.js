0b1c49b3d5f138c88d939fb87513a17bcd2f95e6
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-25 11:52:59 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, {Fragment} from "react";
-import "tachyons";
-import cn from 'classname'
+import React, {Fragment} from 'react';
+import 'tachyons';
+import cn from 'classname';
 
-function RepeatString({ value = "default string", times = 30 }) {
+function RepeatString({ value = 'default string', times = 30 }) {
   return (
     <div>
       {new Array(times).fill(value).map((s, i) => <div key={i}>{s}</div>)}
@@ -10,28 +10,48 @@ function RepeatString({ value = "default string", times = 30 }) {
   );
 }
 function HeaderContent() {
-  return <div className={"f2 ma2"}>fixed header</div>;
+  return <div className={'f2 ma2'}>fixed header</div>;
 }
 
-
-function DrawerLayout({ className,children, header, footer, sidebar, debug=false }) {
+function DrawerLayout({
+  className,
+  children,
+  header,
+  footer,
+  sidebar,
+  debug = false,
+}) {
   return (
     <Fragment>
-      <div className={cn("flex flex-column vh-100 overflow-hidden ", className,{'bg-black-20':debug})}>
-        <div className={cn({"bg-black-20 ba bw1 b--blue":debug})}>{header}</div>
-        <div className={"flex-auto flex "}>
-          {sidebar && <div
-            className={cn("w-30 mw6 overflow-scroll",{"bg-black-40 ba bw1 b--pink":debug})}
-          >
-            {sidebar}
-          </div>}
+      <div
+        className={cn('flex flex-column vh-100 overflow-hidden ', className, {
+          'bg-black-20': debug,
+        })}
+      >
+        <div className={cn({ 'bg-black-20 ba bw1 b--blue': debug })}>
+          {header}
+        </div>
+        <div className={'flex-auto flex '}>
+          {sidebar && (
+            <div
+              className={cn('w-30 mw6 overflow-scroll', {
+                'bg-black-40 ba bw1 b--pink': debug,
+              })}
+            >
+              {sidebar}
+            </div>
+          )}
           <div
-            className={cn("flex-auto overflow-scroll ",{"bg-black-50 ba bw1 b--green":debug})}
+            className={cn('flex-auto overflow-scroll ', {
+              'bg-black-50 ba bw1 b--green': debug,
+            })}
           >
             {children}
           </div>
         </div>
-        <div className={cn({"bg-black-20 ba bw1 b--red":debug})}>{footer}</div>
+        <div className={cn({ 'bg-black-20 ba bw1 b--red': debug })}>
+          {footer}
+        </div>
       </div>
     </Fragment>
   );
@@ -40,13 +60,13 @@ function DrawerLayout({ className,children, header, footer, sidebar, debug=false
 function App() {
   return (
     <DrawerLayout
-      className={cn('bg-near-white')}
-    header={<HeaderContent />}
-    // footer={<div className={"pa2 f4"}>fixed footer</div>}
-    sidebar={<RepeatString value={"drawer item"} times={3} />}
-    debug={false}
+      className='bg-near-white sans-serif'
+      header={<HeaderContent />}
+      // footer={<div className={"pa2 f4"}>fixed footer</div>}
+      sidebar={<RepeatString value={'drawer item'} times={3} />}
+      debug={false}
     >
-      <RepeatString value={"main content items"} times={3} />
+      <RepeatString value={'main content items'} times={3} />
     </DrawerLayout>
   );
 }
--- src/components/PageFooter.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from "react";
-
-class PageFooter extends Component {
-  render() {
-    return <div>Page Footer</div>;
-  }
-}
-
-export default PageFooter;
