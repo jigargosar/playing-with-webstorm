7b26b069c5466a734e9d809ddfcc8bccd25a8e52
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-25 12:19:34 +0530

    init

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -2,13 +2,13 @@ import {DrawerLayout} from '../shared-components/DrawerLayout'
 import React from 'react'
 import {PropTypes} from '../lib-exports/PropTypes'
 
-export function MainLayout({ title = 'Playing With Webstorm', ...otherProps }) {
+export function MainLayout({ children, title = 'Playing With Webstorm', ...otherProps }) {
   return (
     <DrawerLayout
       header={<div className={'f2 ma2'}>{title}</div>}
       // footer={<div className={"pa2 f4"}>fixed footer</div>}
       {...otherProps}
-    />
+     children={children}/>
   )
 }
 
