e8981cd51f9ac81750bef249a5f026fe39d15573
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-25 10:35:44 +0530

    init

--- src/App.js
+++ src/App.js
@@ -92,18 +92,18 @@ function App() {
   return (
     <Fragment>
       <div className={"flex flex-column vh-100 overflow-hidden bg-black-20"}>
-        <div className={"bg-black-20"}>
+        <div className={"bg-black-20 ba bw1 b--blue"}>
           <HeaderContent />
         </div>
         <div className={"flex-auto flex "}>
-          <div className={"w-30 mw6 overflow-scroll bg-black-40 outline"}>
+          <div className={"w-30 mw6 overflow-scroll bg-black-40 ba bw1 b--pink"}>
             <RepeatString value={"side bar item"} times={3}/>
           </div>
           <div className={"flex-auto overflow-scroll bg-black-50 ba bw1 b--green"}>
             <RepeatString value={"main content items"} times={3}/>
           </div>
         </div>
-        <div className={"bg-black-20"}>
+        <div className={"bg-black-20 ba bw1 b--red"}>
           <FooterContent />
         </div>
       </div>
