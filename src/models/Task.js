3680bd8428556d775f164800f7b0c27e220b0b7d
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-25 18:27:55 +0530

    init

--- src/models/Task.js
+++ src/models/Task.js
@@ -1,5 +1,11 @@
-import * as nanoid from "nanoid";
+import * as nanoid from 'nanoid'
+import {randomWords} from '../lib/fake'
 
-export function Task({id=`task/${nanoid()}`, title, done, dueAt, createdAt}={}) {
-  return {id,title,done,dueAt,createdAt}
+export function Task({
+  id = `task/${nanoid()}`,
+  title = randomWords(),
+  done = false,
+  createdAt = Date.now(),
+} = {}) {
+  return { id, title, done, dueAt, createdAt }
 }
