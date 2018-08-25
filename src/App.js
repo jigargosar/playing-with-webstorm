e868fea7bfcc1b27317cd43c925fc40dc72929d3
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-25 18:48:39 +0530

    init

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from 'react'
 import 'tachyons'
 import {MainLayout} from './components/MainLayout'
 import {getTitle, Task} from './models/Task'
+import {times} from 'ramda'
 
 function App() {
   return (
     <MainLayout title={'FunDo'}>
-      {new Array(10)
-        .fill(null)
-        .map((s, i) => <div key={i}>{getTitle(Task())}</div>)}
+      {times(i => <div key={i}>{getTitle(Task())}</div>, 30)}
     </MainLayout>
   )
 }
---
+++ src/lib-exports/ramda-exports.js
@@ -0,0 +1,3 @@
+import times from 'ramda/times'
+
+export {times}
