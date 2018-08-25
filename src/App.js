b627d4e1a2c33be1e099b6ef7e9ba10122640710
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-25 09:50:35 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,87 +1,160 @@
-import React, {Component, Fragment} from "react";
+import React from "react";
 import "tachyons";
 
-class App extends Component {
-  render() {
-    return (
-      <Fragment>
-        <div className={'vh-100 bg-black-30'}>
-          <div>
-            fixed header
-          </div>
-          <div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-            <div>content</div>
-          </div>
-          <div>
-            fixed footer
-          </div>
+function MainContent() {
+  return (
+    <div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+      <div>content</div>
+    </div>
+  );
+}
+function SidebarContent() {
+  return (
+    <div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+      <div>sidebar</div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className={'vh-100 overflow-hidden'}>
+      <div className={"flex h-100 flex-column overflow-scroll bg-black-30"}>
+        <div className={"bg-black-30"}>fixed header</div>
+        <div className={"flex overflow-scroll"}>
+          <div className={'overflow-scroll bg-black-30'}><SidebarContent /></div>
+          <div className={'overflow-scroll'}><MainContent /></div>
         </div>
-      </Fragment>
-    );
-  }
+        <div className={'bg-black-30'}>fixed footer</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
