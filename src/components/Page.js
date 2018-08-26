1f9d3afe1b9d241a8c328a7c00ee47546ca5c7dd
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-26 09:50:00 +0530

    WIP

--- src/components/Page.js
+++ src/components/Page.js
@@ -6,7 +6,7 @@ export const Page = function Page() {
   return (
     <ViewportHeightContainer>
       <h1>STATIC</h1>
-      <div className="overflow-scroll pa3 ma3 ba b--silver">
+      <div className="overflow-scroll ma3 ba b--silver">
         <div className="ma3 pa5 bg-light-purple">A</div>
         <div className="ma3 pa5 bg-light-blue">B</div>
         <div className="pa5 bg-light-red">C</div>
