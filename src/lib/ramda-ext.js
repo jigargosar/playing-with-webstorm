c1c314f5b073f56116de6b7b5bf4c3856f73366a
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-26 18:31:39 +0530

    WIP

--- src/lib/ramda-ext.js
+++ src/lib/ramda-ext.js
@@ -38,3 +38,10 @@ export const toggleProp = propName => overProp(propName)(vNot)
 
 export const overItemInListWithId = id => fn => cName =>
   overProp(cName)(overModelWithId(id)(fn))
+
+export const overFst = overIndex(0)
+export const overSnd = overIndex(1)
+
+export const mapOverFst = mapOverNth(0)
+export const mapOverSnd = mapOverNth(1)
+export const mapOverNth = n => fn => map(overIndex(n)(fn))
