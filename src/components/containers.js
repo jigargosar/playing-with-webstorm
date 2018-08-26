9a6294f0ae7ab69c68426d26d4885797c0a82ff0
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-26 19:51:49 +0530

    WIP

--- src/components/containers.js
+++ src/components/containers.js
@@ -4,9 +4,9 @@ export const FullHeightContainer = withProps({
   className: 'h-100 overflow-hidden flex flex-column',
 })('div')
 
-export const ViewportHeightContainer = withProps({
-  className: 'vh-100 overflow-hidden flex flex-column',
-})('div')
+export const ViewportHeightContainer = withProps(({ className }) => ({
+  className: `vh-100 overflow-hidden flex flex-column ${className}`,
+}))('div')
 
 export const ScrollContainer = withProps({
   className: 'overflow-scroll flex-grow-1',
