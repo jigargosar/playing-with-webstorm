9682c31f255e045475a122485f5dfca95bda065e
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-26 09:38:59 +0530

    WIP

--- src/components/Page.js
+++ src/components/Page.js
@@ -4,14 +4,16 @@ import React from 'react'
 export const Page = function Page() {
   return (
     <div className="vh-100">
-      <div className="h-100 overflow-hidden">
-        <div className="flex flex-column">
+      <div className="h-100 overflow-hidden flex flex-column">
+        <h1>STATIC</h1>
+        <div className="overflow-scroll">
           <div className="pa5">A</div>
           <div className="pa5">B</div>
           <div className="pa5">C</div>
           <div className="pa5">D</div>
           <div className="pa5">E</div>
         </div>
+        <h1>STATIC</h1>
       </div>
     </div>
   )
