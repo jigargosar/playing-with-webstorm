b44de1b72e012a23c0a08d6a49fc84eb88c6f42f
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-26 09:48:54 +0530

    WIP

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { ViewportHeightContainer } from './containers'
+import { FullHeightContainer } from './containers'
 // import cn from "classname";
 
 export const Page = function Page() {
   return (
-    <ViewportHeightContainer>
+    <FullHeightContainer>
       <h1>STATIC</h1>
       <div className="overflow-scroll pa3 ma3 ba b--silver">
         <div className="ma3 pa5 bg-light-purple">A</div>
@@ -14,6 +14,6 @@ export const Page = function Page() {
         <div className="pa5">E</div>
       </div>
       <h1>STATIC</h1>
-    </ViewportHeightContainer>
+    </FullHeightContainer>
   )
 }
