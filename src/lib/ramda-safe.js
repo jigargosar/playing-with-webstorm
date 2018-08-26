a5000e7a721eaf12a4a96d9b989ee6b4a111663d
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-26 16:38:29 +0530

    WIP

--- src/lib/ramda-safe.js
+++ src/lib/ramda-safe.js
@@ -1,6 +1,11 @@
 import { validate } from './validate'
 import { not } from 'ramda'
 
+const tapValidateArgs = spec => fn => (...args) => {
+  validate(spec, ...args)
+  return fn(...args)
+}
+
 export const vNot = boolean => {
   validate('B', [boolean])
   return not(boolean)
