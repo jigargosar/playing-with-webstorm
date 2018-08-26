9b533bb7a727f67f1dcc5d28100f04f74fa1c3d3
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-26 09:42:38 +0530

    WIP

--- src/components/Page.js
+++ src/components/Page.js
@@ -6,10 +6,10 @@ export const Page = function Page() {
     <div className="vh-100">
       <div className="h-100 overflow-hidden flex flex-column">
         <h1>STATIC</h1>
-        <div className="overflow-scroll">
-          <div className="pa5">A</div>
-          <div className="pa5">B</div>
-          <div className="pa5">C</div>
+        <div className="overflow-scroll pa3 ma3 ba b--silver">
+          <div className="ma3 pa5 bg-light-purple">A</div>
+          <div className="ma3 pa5 bg-light-blue">B</div>
+          <div className="pa5 bg-light-red">C</div>
           <div className="pa5">D</div>
           <div className="pa5">E</div>
         </div>
