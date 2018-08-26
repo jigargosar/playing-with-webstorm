a117c705eee3ef331f4a818259a3e17d03c51052
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-26 09:47:08 +0530

    WIP

--- src/components/Page.js
+++ src/components/Page.js
@@ -6,20 +6,22 @@ const FullHeightContainer = withProps({
   className: 'h-100 overflow-hidden flex flex-column',
 })('div')
 
+const ViewportHeightContainer = withProps({
+  className: 'vh-100 overflow-hidden flex flex-column',
+})('div')
+
 export const Page = function Page() {
   return (
-    <div className="vh-100">
-      <FullHeightContainer>
-        <h1>STATIC</h1>
-        <div className="overflow-scroll pa3 ma3 ba b--silver">
-          <div className="ma3 pa5 bg-light-purple">A</div>
-          <div className="ma3 pa5 bg-light-blue">B</div>
-          <div className="pa5 bg-light-red">C</div>
-          <div className="pa5">D</div>
-          <div className="pa5">E</div>
-        </div>
-        <h1>STATIC</h1>
-      </FullHeightContainer>
-    </div>
+    <ViewportHeightContainer>
+      <h1>STATIC</h1>
+      <div className="overflow-scroll pa3 ma3 ba b--silver">
+        <div className="ma3 pa5 bg-light-purple">A</div>
+        <div className="ma3 pa5 bg-light-blue">B</div>
+        <div className="pa5 bg-light-red">C</div>
+        <div className="pa5">D</div>
+        <div className="pa5">E</div>
+      </div>
+      <h1>STATIC</h1>
+    </ViewportHeightContainer>
   )
 }
