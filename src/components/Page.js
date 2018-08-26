26a1dbcd824b7f5dd43b8b652f7f0e858b313eb5
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-26 10:03:48 +0530

    WIP

--- src/components/Page.js
+++ src/components/Page.js
@@ -14,32 +14,34 @@ const enhance = compose(
   setDisplayName('Page'),
 )
 
-export const Page = enhance(function Page() {
-  return (
-    <ViewportHeightContainer>
-      <header>
-        <h1>STATIC HEADER</h1>
-      </header>
-      <div className="overflow-scroll ma3 ba b--silver">
-        <div className="ma3 pa3 bg-light-purple">A</div>
-        <div className="ma3 pa3 bg-light-blue">B</div>
-        <div className="pa3 bg-light-red">C</div>
-        <div className="pa3">D</div>
-        <div className="pa3">E</div>
-      </div>
-      <header>
-        <h1>STATIC Content</h1>
-      </header>
-      <div className="overflow-scroll ma3 ba b--silver">
-        <div className="ma3 pa3 bg-light-purple">A</div>
-        <div className="ma3 pa3 bg-light-blue">B</div>
-        <div className="pa3 bg-light-red">C</div>
-        <div className="pa3">D</div>
-        <div className="pa3">E</div>
-      </div>
-      <footer>
-        <h1>STATIC FOOTER</h1>
-      </footer>
-    </ViewportHeightContainer>
-  )
-})
+export const Page =
+  //
+  enhance(function Page() {
+    return (
+      <ViewportHeightContainer>
+        <header>
+          <h1>STATIC HEADER</h1>
+        </header>
+        <div className="overflow-scroll ma3 ba b--silver">
+          <div className="ma3 pa3 bg-light-purple">A</div>
+          <div className="ma3 pa3 bg-light-blue">B</div>
+          <div className="pa3 bg-light-red">C</div>
+          <div className="pa3">D</div>
+          <div className="pa3">E</div>
+        </div>
+        <header>
+          <h1>STATIC Content</h1>
+        </header>
+        <div className="overflow-scroll ma3 ba b--silver">
+          <div className="ma3 pa3 bg-light-purple">A</div>
+          <div className="ma3 pa3 bg-light-blue">B</div>
+          <div className="pa3 bg-light-red">C</div>
+          <div className="pa3">D</div>
+          <div className="pa3">E</div>
+        </div>
+        <footer>
+          <h1>STATIC FOOTER</h1>
+        </footer>
+      </ViewportHeightContainer>
+    )
+  })
