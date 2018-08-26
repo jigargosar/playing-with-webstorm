434b80ab3679f965345eaa58e24d540036ec4cf9
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-26 09:52:11 +0530

    WIP

--- src/components/Page.js
+++ src/components/Page.js
@@ -9,11 +9,11 @@ export const Page = function Page() {
         <h1>STATIC HEADER</h1>
       </header>
       <div className="overflow-scroll ma3 ba b--silver">
-        <div className="ma3 pa5 bg-light-purple">A</div>
-        <div className="ma3 pa5 bg-light-blue">B</div>
-        <div className="pa5 bg-light-red">C</div>
-        <div className="pa5">D</div>
-        <div className="pa5">E</div>
+        <div className="ma3 pa3 bg-light-purple">A</div>
+        <div className="ma3 pa3 bg-light-blue">B</div>
+        <div className="pa3 bg-light-red">C</div>
+        <div className="pa3">D</div>
+        <div className="pa3">E</div>
       </div>
       <header>
         <h1>STATIC Content</h1>
