0e414444c3063da1735f858041a7e9089e94938a
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-26 16:40:50 +0530

    WIP

--- src/lib/ramda-ext.js
+++ src/lib/ramda-ext.js
@@ -7,7 +7,6 @@ import {
   over,
   propEq,
 } from 'ramda'
-import { assert } from './assert'
 import { vNot } from './ramda-safe'
 
 const findIndexById = id => findIndex(propEq('id', id))
@@ -15,12 +14,9 @@ const findIndexById = id => findIndex(propEq('id', id))
 export const overItemById = curry((id, fn, list) =>
   over(lensIndex(findIndexById(id)(list)))(fn)(list),
 )
-export const overModel = curry(({ id }, fn, list) => {
-  const taskIdx = findIndex(propEq('id', id))(list)
-  assert(taskIdx !== -1)
-  return over(lensIndex(taskIdx), fn, list)
-})
+
 export const findById = id => find(propEq('id', id))
+
 export const overProp = propName => over(lensProp(propName))
 
 export const toggleProp = propName => overProp(propName)(vNot)
