65015b6456aecd8117d1ab9e800b916310237120
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-26 17:19:19 +0530

    WIP

--- src/lib/ramda-ext.js
+++ src/lib/ramda-ext.js
@@ -9,26 +9,24 @@ import {
 } from 'ramda'
 import { vNot } from './ramda-safe'
 
-const idEq = propEq('id')
+export const idEq = propEq('id')
 
 export const findById = compose(
   find,
   idEq,
 )
 
-export const findIndexById =
-  //
-  compose(
-    findIndex,
-    idEq,
-  )
+export const findIndexById = compose(
+  findIndex,
+  idEq,
+)
 
-export const overModelWithId = (id) => (fn) => (list) => {
+export const overModelWithId = id => fn => list => {
   const idx = findIndexById(id)(list)
 
   return over(lensIndex(idx))(fn)(list)
 }
 
-export const overProp = (propName) => over(lensProp(propName))
+export const overProp = propName => over(lensProp(propName))
 
-export const toggleProp = (propName) => overProp(propName)(vNot)
+export const toggleProp = propName => overProp(propName)(vNot)
