9f239819cae9362fb9e1d7ede2b6e6caa9027af0
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-26 17:44:56 +0530

    WIP

--- src/lib/ramda-ext.test.js
+++ src/lib/ramda-ext.test.js
@@ -1,9 +1,17 @@
 import { findIndexById } from './ramda-ext'
+import { toPairs } from 'ramda'
 
 describe('findIndexById', function() {
   it('should find matching element', function() {
     const id1Element = { id: 'id1' }
     const idx = findIndexById('id1')([id1Element])
     expect(idx).toBe(0)
+  })
+})
+
+describe('arrayToPairs', function() {
+  it('should convert array to index,obj pair', function() {
+    const indexPairs = toPairs(['a', 'b'])
+    expect(indexPairs).toEqual([['0', 'a'], ['1', 'b']])
   })
 })
