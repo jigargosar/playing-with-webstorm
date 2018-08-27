41cd61c6cb19e6db633f5aa30551fccceb3560ec
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-27 14:47:19 +0530

    WIP

--- src/components/Page.js
+++ src/components/Page.js
@@ -78,8 +78,8 @@ const TaskList = enhanceTaskList(function TaskList({
   effects,
 }) {
   return (
-    <div className="center measure-wide">
-      <div className="ma3 pa3 br3 bg-white shadow-1 ">
+    <div className="center measure-wide mv3">
+      <div className="pa3 br3 bg-white shadow-1 ">
         <div className="">Tasks</div>
         <Models models={tasks}>{task => <Task task={task} />}</Models>
       </div>
