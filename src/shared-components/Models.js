1a9686693488401983b146fe9867a53cf581a8dc
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-27 17:59:04 +0530

    WIP

--- src/shared-components/Models.js
+++ src/shared-components/Models.js
@@ -1,11 +1,15 @@
 import React, { Fragment } from 'react'
 import * as PropTypes from 'prop-types'
+import * as xr from 'mobx-react'
 
-export function Models({ models, children: render }) {
+export const Models = xr.observer(function Models({
+  models,
+  children: render,
+}) {
   return models.map((model, index) => (
     <Fragment key={model.id}>{render(model, index)}</Fragment>
   ))
-}
+})
 
 Models.propTypes = {
   models: PropTypes.array.isRequired,
