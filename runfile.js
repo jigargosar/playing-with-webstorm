f0c530fdf6e8500bf1c6de08401bb717177cf557
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-27 10:32:19 +0530

    WIP

--- runfile.js
+++ runfile.js
@@ -1,4 +1,7 @@
 import { run } from 'runjs'
+import { assert } from './src/lib/assert'
+
+process.env['REACT_EDITOR'] = 'webstorm'
 
 export function hello(name = 'Mysterious') {
   console.log(`Hello ${name}!`)
@@ -11,3 +14,13 @@ export function installCommonPackages() {
 }
 
 export const i = installCommonPackages
+
+export function rewired(cmdName, ...options) {
+  assert(['start', 'build', 'test'].includes(cmdName))
+  run(`react-app-rewired ${cmdName}`)
+}
+
+export const dev = () => {
+  var cmdName = 'start'
+  rewired(cmdName)
+}
