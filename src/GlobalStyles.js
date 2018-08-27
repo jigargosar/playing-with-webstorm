77fe447d970190f49003c2a71f61178762116e6e
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-27 16:52:36 +0530

    WIP

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -1,10 +1,14 @@
-import React, { Fragment } from 'react'
-import { Style } from 'radium'
+//
 import tachyonsRules from '!radium-loader!css-loader!tachyons' // eslint-disable-line
 import indexRules from '!radium-loader!css-loader!./index.css' // eslint-disable-line
+//
+import React, { Fragment } from 'react'
+import { Style } from 'radium'
+import { __, pick } from 'ramda'
 
 const tr = tachyonsRules
-export { tr, indexRules }
+const trs = pick(__)(tr)
+export { tr, trs, indexRules }
 
 export function GlobalStyles() {
   return (
