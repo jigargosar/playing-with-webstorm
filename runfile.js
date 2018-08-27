9f7a65f2fe6c7f4a3de1ce265d6a45e2a3bffafe
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-27 10:18:30 +0530

    WIP

--- runfile.js
+++ runfile.js
@@ -4,6 +4,8 @@ export function hello(name = 'Mysterious') {
   console.log(`Hello ${name}!`)
 }
 
-function installCommonPackages() {
-  run('yarn add ')
+export function installCommonPackages() {
+  run(
+    'yarn add -D react-app-rewired power-assert babel-plugin-empower-assert babel-plugin-espower',
+  )
 }
