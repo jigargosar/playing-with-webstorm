3c878c17bf07cee63234d0fc32defe3377d286b1
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-27 17:46:43 +0530

    WIP

---
+++ src/components/withMobX.js
@@ -0,0 +1,29 @@
+import * as x from 'mobx'
+import { createNewTaskWithDefaults } from '../models/Task'
+import { clamp, isEmpty, times } from 'ramda'
+import { findIndexById } from '../lib/ramda-ext'
+
+export const state = x.observable.object({
+  tasks: times(createNewTaskWithDefaults)(16),
+  sIdx: 0,
+
+  get clampedSIdx() {
+    if (isEmpty(state.tasks)) return NaN
+    return clamp(0, state.tasks.length - 1)(state.sIdx)
+  },
+  get selectedTask() {
+    return state.tasks[state.clampedSIdx]
+  },
+})
+
+export const handleSelectTask = id => () => {
+  state.sIdx = findIndexById(id)(state.tasks)
+}
+
+export const handleSelectedTaskToggleDone = () => {
+  const selectedTask = state.selectedTask
+  selectedTask.done = !selectedTask.done
+}
+export const handleSelectedTaskDelete = id => () => {
+  state.tasks.splice(state.clampedSIdx, 1)
+}
