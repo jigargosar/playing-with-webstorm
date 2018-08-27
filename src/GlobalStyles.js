305fbd0503d8204757af7b540a649f52e54d4b64
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-27 16:14:05 +0530

    WIP

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -3,6 +3,8 @@ import Style from 'radium/es/components/style'
 import tachyonsRules from 'tachyons'
 import indexRules from './index.css'
 
+export { tachyonsRules, indexRules }
+
 export function GlobalStyles() {
   return (
     <Fragment>
