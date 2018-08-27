2a6392066d029c924cffc6eb2e79c363698e61cf
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-27 17:47:48 +0530

    WIP

--- src/components/withMobX.js
+++ src/components/withMobX.js
@@ -19,10 +19,9 @@ export const state = x.observable.object({
 export const handleSelectTask = id => () => {
   state.sIdx = findIndexById(id)(state.tasks)
 }
-
 export const handleSelectedTaskToggleDone = () => {
-  const selectedTask = state.selectedTask
-  selectedTask.done = !selectedTask.done
+  const task = state.selectedTask
+  task.done = !task.done
 }
 export const handleSelectedTaskDelete = id => () => {
   state.tasks.splice(state.clampedSIdx, 1)
