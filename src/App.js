f5703695ff0809c32b54d8412cc082d5b4e97e4e
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-27 16:07:09 +0530

    WIP

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Page } from './components/Page'
+import { Style } from 'radium'
+
+import tachyonsRules from '!radium-loader!css-loader!tachyons' // eslint-disable-line import/no-webpack-loader-syntax
+import indexRules from '!radium-loader!css-loader!./index.css' // eslint-disable-line import/no-webpack-loader-syntax
 
 class App extends Component {
   state = {
     hasError: false,
   }
   render() {
-    return this.state.hasError ? 'Error' : <Page />
+    return (
+      <Fragment>
+        <Style rules={tachyonsRules} />
+        <Style rules={indexRules} />
+        {this.state.hasError ? 'Error' : <Page />}
+      </Fragment>
+    )
   }
 
   componentDidCatch(error, info) {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import 'tachyons'
+
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+// import { Style } from 'radium';
+
+// import './index.css'
+// import 'tachyons'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
