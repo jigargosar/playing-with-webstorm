69b85e566f5510c316c0d0bb14b205fb531856e3
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-27 15:18:59 +0530

    WIP

--- src/components/Page.js
+++ src/components/Page.js
@@ -9,10 +9,7 @@ import { injectState, withAppState } from './withFreactal'
 
 function FloatingActionsContainer({ children }) {
   return (
-    <div
-      className="absolute z-1 flex items-center "
-      style={{ right: '0.5rem' }}
-    >
+    <div className="absolute z-1 flex items-center " style={{ right: '2rem' }}>
       <div className="absolute ">
         <div className="pa2 bg-white-80 br3 shadow-1">{children}</div>
       </div>
