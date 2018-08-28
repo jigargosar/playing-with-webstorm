6e280e79caf66436f20afabdf045f2a617991913
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-28 23:18:55 +0530

    WIP

--- src/components/Page.js
+++ src/components/Page.js
@@ -39,7 +39,6 @@ const FloatingActionsContainer = Radium(function FloatingActionsContainer({
 })
 
 const TaskContent = observer(function TaskContent({ task: { done, title } }) {
-  console.log('done', done)
   return (
     <div
       className={cn('flex-auto pa2 f5 bg-light-purple br2', {
