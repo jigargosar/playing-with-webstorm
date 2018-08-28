716e081bc0cd0f93f73219d39f935e833bde3818
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-28 23:57:46 +0530

    WIP

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,6 @@
 import { tr } from './GlobalStyles'
 
 export const secondaryColor = tr['.orange']
-export const primaryBgColor = tr['.bg-light-purple']
+const primaryColorName = `light-purple`
+export const primaryColor = tr[`.${primaryColorName}`]
+export const primaryBgColor = tr[`.bg-${primaryColorName}`]
