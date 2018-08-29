6a7b3bb809cd4c90e58ca368de005cf5f3bef619
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-29 12:07:26 +0530

    WIP

--- src/components/xUtils.js
+++ src/components/xUtils.js
@@ -3,9 +3,7 @@ import { curry, curryN } from 'ramda'
 import dset from 'dset'
 import dget from 'dlv'
 
-export function xToggleProp(p, task) {
-  task[p] = !task[p]
-}
+export const xToggleProp = curry((p, task) => (task[p] = !task[p]))
 
 export const xRemoveAt = curry((idx, list) => list.splice(idx, 1))
 
