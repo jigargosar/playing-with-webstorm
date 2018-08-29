59ca3cb8960dca2fc7acbd7754b638481155b2e6
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-29 12:14:50 +0530

    WIP

--- src/components/xUtils.js
+++ src/components/xUtils.js
@@ -3,6 +3,7 @@ import { curry, curryN } from 'ramda'
 import dset from 'dset'
 import dget from 'dlv'
 import { validate } from '../lib/validate'
+import { findIndexById } from '../lib/ramda-ext'
 
 export const xToggleProp = curry((p, task) => (task[p] = !task[p]))
 
@@ -11,6 +12,9 @@ export const xRemoveAt = curry((idx, list) => {
   return list.splice(idx, 1)
 })
 
+const xRemoveById = curry((id, list) =>
+  xRemoveAt(findIndexById(id)(list))(list),
+)
 export const xSet = curryN(3, dset)
 export const xGet = curryN(2, dget)
 export const xGetOr = curryN(3, dget)
