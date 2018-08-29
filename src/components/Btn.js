a277054d21efe725ed60e5cc1c98a59a6635710c
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-29 12:26:32 +0530

    WIP

--- src/components/Btn.js
+++ src/components/Btn.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Btn = function FloatingAction(props) {
   return (
     <button
-      className={'code mh1 ph1 orange'}
+      className={'code mh1 ph1 orange link'}
       style={{
         background: 'transparent',
         userSelect: 'none',
