934ee5b8ed3b33473bd969e02feca5e64008c9cb
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-29 12:22:36 +0530

    WIP

--- src/components/Btn.js
+++ src/components/Btn.js
@@ -1,24 +1,16 @@
-import Radium from 'radium'
 import React from 'react'
-import { secondaryColor } from '../theme'
 
-const buttonStyle = {
-  base: [
-    {
-      background: 'transparent',
-      userSelect: 'none',
-      border: 'none',
-      cursor: 'pointer',
-    },
-  ],
-  warning: [secondaryColor],
-}
-export const Btn = Radium(function FloatingAction(props) {
+export const Btn = function FloatingAction(props) {
   return (
     <button
-      className={'code mh1 ph1'}
-      style={[buttonStyle.base, buttonStyle.warning]}
+      className={'code mh1 ph1 orange'}
+      style={{
+        background: 'transparent',
+        userSelect: 'none',
+        border: 'none',
+        cursor: 'pointer',
+      }}
       {...props}
     />
   )
-})
+}
