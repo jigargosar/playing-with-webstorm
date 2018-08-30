4a82676a5add204bd059b1fe23cc6e298e2015d1
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-30 20:43:45 +0530

    WIP

--- src/lib/ramda-strict.js
+++ src/lib/ramda-strict.js
@@ -1,5 +1,5 @@
 import { validate } from './validate'
-import { curry, defaultTo, map as _map, not as _not, path } from 'ramda'
+import { curry, path, pathOr } from 'ramda'
 
 const validateIO = (inputSpecs, outputSpecs = '*') => fn => (...args) => {
   validate(inputSpecs, args)
@@ -8,25 +8,7 @@ const validateIO = (inputSpecs, outputSpecs = '*') => fn => (...args) => {
   return result
 }
 
-export const not = validateIO('B')(_not)
-export const map = validateIO('O|A')(_map)
-
-export const defaultToS = curry((def, val) => {
-  validate('SS|SZ', [def, val])
-  return defaultTo(def, val)
-})
-
-export const pathS = curry(
-  validateIO('AO')((paths, obj) => {
-    const result = path(paths, obj)
-    validate('S', [result])
-    return result
-  }),
-)
-export const pathSOr = curry(
-  validateIO('SAO')((def, paths, obj) => {
-    return defaultToS(def)(path(paths, obj))
-  }),
-)
+export const pathS = curry(validateIO('AO', 'S')(path))
+export const pathSOr = curry(validateIO('SAO', 'S|Z')(pathOr))
 export const propS = curry((p, obj) => pathS([p], obj))
 export const propSOr = curry((def, p, obj) => pathSOr(def, [p], obj))
