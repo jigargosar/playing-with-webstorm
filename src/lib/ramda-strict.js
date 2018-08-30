3c455a7a50edb43fac5d55571298662c872807c5
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-30 20:48:11 +0530

    WIP

--- src/lib/ramda-strict.js
+++ src/lib/ramda-strict.js
@@ -12,5 +12,7 @@ const validateIO = (inputSpecs, outputSpecs = '*') => fn => {
 
 export const pathS = validateIO('AO', 'S')(path)
 export const pathSOr = validateIO('SAO', 'S|Z')(pathOr)
-export const propS = (p, obj) => pathS([p], obj)
-export const propSOr = (def, p, obj) => pathSOr(def, [p], obj)
+export const propS = validateIO('SO', 'S')((p, obj) => pathS([p], obj))
+export const propSOr = validateIO('SSO', 'S')((def, p, obj) =>
+  pathSOr(def, [p], obj),
+)
