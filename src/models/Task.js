73636403f2ef951ad4742dc3f680464ff5ffc54e
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-30 19:43:23 +0530

    WIP

--- src/models/Task.js
+++ src/models/Task.js
@@ -2,16 +2,17 @@ import * as nanoid from 'nanoid'
 import { randomWords } from '../lib/fake'
 import { validate } from '../lib/validate'
 
-export function createTask({
-  id = `task_${nanoid()}`,
-  title = randomWords(),
-  done = false,
-  createdAt = Date.now(),
-} = {}) {
+function Task({ id, title, done, createdAt }) {
   validate('SSBN', [id, title, done, createdAt])
   return { id, title, done, createdAt }
 }
 
 export function createNewTaskWithDefaults() {
-  return createTask()
+  const defaults = {
+    id: `task_${nanoid()}`,
+    title: randomWords(),
+    done: false,
+    createdAt: Date.now(),
+  }
+  return Task(defaults)
 }
