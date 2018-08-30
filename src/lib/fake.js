df23d01682e334c588bc18636f449e7673ad8e30
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-30 19:45:17 +0530

    WIP

--- src/lib/fake.js
+++ src/lib/fake.js
@@ -1,13 +1,12 @@
 import faker from 'faker'
-import { Chance } from 'chance'
 
 import jsf from 'json-schema-faker'
 import nanoid from 'nanoid'
 
-const seed = Math.random()
-
-export const chance = Chance(seed)
-faker.seed(seed)
+// export const chance = Chance()
+// const seed = Math.random()
+// export const chance = Chance(seed)
+// faker.seed(seed)
 
 export function randomWords() {
   return faker.random.word()
