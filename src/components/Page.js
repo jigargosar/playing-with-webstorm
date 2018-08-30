7ce9b211169cd6c490df423f2b4fe8a74c74fed1
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-30 08:39:12 +0530

    WIP

--- src/components/Page.js
+++ src/components/Page.js
@@ -98,7 +98,7 @@ const MainContent = composeHOC()(function MainContent() {
   return (
     <div className="center measure-wide mv3">
       <div className="pa3 br3 bg-white shadow-1 ">
-        <div className="">Tasks</div>
+        <Flex marginBottom={'0.5rem'}>Tasks</Flex>
         <TaskItems />
       </div>
     </div>
