e55b6d16be10b480f79cfed4b8e8315c3c87b09a
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-30 20:37:23 +0530

    WIP

--- src/lib/ramda-strict.js
+++ src/lib/ramda-strict.js
@@ -1,13 +1,13 @@
 import { validate } from './validate'
 import { curry, defaultTo, map as _map, not as _not, path } from 'ramda'
 
-const tapValidateArgs = spec => fn => (...args) => {
+const validateArgs = spec => fn => (...args) => {
   validate(spec, args)
   return fn(...args)
 }
 
-export const not = tapValidateArgs('B')(_not)
-export const map = tapValidateArgs('O|A')(_map)
+export const not = validateArgs('B')(_not)
+export const map = validateArgs('O|A')(_map)
 
 export const defaultToS = curry((def, val) => {
   validate('SS|SZ', [def, val])
@@ -15,14 +15,14 @@ export const defaultToS = curry((def, val) => {
 })
 
 export const pathS = curry(
-  tapValidateArgs('AO')((paths, obj) => {
+  validateArgs('AO')((paths, obj) => {
     const result = path(paths, obj)
     validate('S', [result])
     return result
   }),
 )
 export const pathSOr = curry(
-  tapValidateArgs('SAO')((def, paths, obj) => {
+  validateArgs('SAO')((def, paths, obj) => {
     return defaultToS(def)(path(paths, obj))
   }),
 )
