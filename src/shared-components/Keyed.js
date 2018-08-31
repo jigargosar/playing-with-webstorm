01ea148fc4462d9608e97be6c6c77da69f705a2a
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-31 13:27:18 +0530

    WIP

--- src/shared-components/Keyed.js
+++ src/shared-components/Keyed.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react'
 import * as PropTypes from 'prop-types'
 import { propS } from '../lib/ramda-strict'
 import { observer } from 'mobx-react'
-import { addIndex, always, compose, map, nth } from 'ramda'
+import { addIndex, always, map } from 'ramda'
 
 export const Keyed = observer(function Keyed({
   list,
@@ -27,9 +27,6 @@ Keyed.propTypes = {
 }
 
 Keyed.defaultProps = {
-  getKey: compose(
-    propS('id'),
-    nth(0),
-  ),
+  getKey: e => propS('id')(e),
   getProps: always({}),
 }
