3b0fa9cdd7993ce5de8437a22531eef82f401649
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-31 16:49:40 +0530

    WIP

--- src/components/TasksPage/elements/Task.js
+++ src/components/TasksPage/elements/Task.js
@@ -1,17 +1,12 @@
-import React from 'react'
 import PropTypes from 'prop-types'
 import { Base, styled } from 'reakit'
 import { ifProp } from 'styled-tools'
 import { primaryDark, primaryLight } from '../../../reakit-components'
 
-const TaskStyled = styled(Base)`
+const Task = styled(Base).attrs({ className: 'mv2 pv2 br2' })`
   ${ifProp('hovered', { backgroundColor: primaryLight })};
   ${ifProp('selected', { color: '#fff', backgroundColor: primaryDark })};
 `
-
-function Task({ selected, hovered, ...otherProps }) {
-  return <TaskStyled className="mv2 pv2 br2" {...otherProps} />
-}
 
 Task.propTypes = {
   hovered: PropTypes.bool,
