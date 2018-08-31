253bcddb38e842e775a3b0f616834371a1014cb3
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-31 15:25:12 +0530

    WIP

--- src/components/TasksPage/Task.js
+++ src/components/TasksPage/Task.js
@@ -52,7 +52,7 @@ export const Task = composeHOC()(function Task({
       <FlexCenter relative>
         {hovered && (
           <FloatingActionsContainer>
-            <Group vertical relative className="pa2 bg-white-90 br3 shadow-1">
+            <Group vertical relative className="_pa2 bg-white-90 br3 shadow-1">
               <Button onClick={linkEvent(toggleTaskDone, task)}>
                 {'Done'}
               </Button>
