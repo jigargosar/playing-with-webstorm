224a6ff1b92e4ffda45b3a7404f3bc143c016978
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-31 16:33:37 +0530

    WIP

--- src/components/TasksPage/elements/Task.js
+++ src/components/TasksPage/elements/Task.js
@@ -2,31 +2,17 @@ import * as PropTypes from 'prop-types'
 import React from 'react'
 import { tap } from 'ramda'
 import { composeHOC } from '../../composeHOC'
-import { Base, Group, Hidden } from 'reakit'
+import { Base } from 'reakit'
 import cn from 'classname'
-import {
-  Button,
-  FlexCenter,
-  FlexColumn,
-  primaryDark,
-  primaryLight,
-} from '../../../reakit-components'
+import { primaryDark, primaryLight } from '../../../reakit-components'
 
 const linkEvent = (fn, ...args) => tap(e => fn(...args, e))
 
 export const Task = composeHOC()(function Task({
-  task,
-  mouseEnterTask,
-  isTaskSelected,
-  isTaskHovered,
-  mouseLeaveTask,
-  selectTask,
-  toggleSelectedTaskDone,
-  toggleTaskDone,
-  deleteTask,
+  selected,
+  hovered,
+  ...otherProps
 }) {
-  const selected = isTaskSelected(task)
-  const hovered = isTaskHovered(task)
   return (
     <Base
       className={cn('mv2 pv2 br2')}
@@ -35,33 +21,11 @@ export const Task = composeHOC()(function Task({
         : hovered
           ? { backgroundColor: primaryLight }
           : {})}
-      onMouseEnter={linkEvent(mouseEnterTask, task)}
-      onMouseLeave={linkEvent(mouseLeaveTask, task)}
-      onClickCapture={linkEvent(selectTask, task)}
-    >
-      <FlexCenter relative>
-        <Hidden visible={hovered} absolute zIndex={1} right={'-4rem'}>
-          <Group vertical relative className="pa2 bg-white-90 br3 shadow-1">
-            <Button onClick={linkEvent(toggleTaskDone, task)}>{'Done'}</Button>
-            <Button onClick={linkEvent(deleteTask, task)}>{'Delete'}</Button>
-          </Group>
-        </Hidden>
-
-        <FlexColumn>
-          <div className={cn('ph2', { strike: task.done })}>{task.title}</div>
-          <small className={'ttu f7 ph2'}>{`@${task.context.title}`}</small>
-        </FlexColumn>
-      </FlexCenter>
-    </Base>
+      {...otherProps}
+    />
   )
 })
 Task.propTypes = {
-  task: PropTypes.shape({ id: PropTypes.string.isRequired }).isRequired,
-  isTaskSelected: PropTypes.func.isRequired,
-  isTaskHovered: PropTypes.func.isRequired,
-  mouseEnterTask: PropTypes.func.isRequired,
-  mouseLeaveTask: PropTypes.func.isRequired,
-  selectTask: PropTypes.func.isRequired,
-  toggleTaskDone: PropTypes.func.isRequired,
-  deleteTask: PropTypes.func.isRequired,
+  hovered: PropTypes.bool,
+  selected: PropTypes.bool,
 }
