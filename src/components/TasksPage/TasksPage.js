67753ac7553ae224e6869c081ddd7c6007577e29
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-31 14:45:06 +0530

    WIP

--- src/components/TasksPage/TasksPage.js
+++ src/components/TasksPage/TasksPage.js
@@ -1,12 +1,63 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { ScrollContainer, ViewportHeightContainer } from '../containers'
 import * as PropTypes from 'prop-types'
 import { store } from '../store'
 import { composeHOC } from '../composeHOC'
-import { Button, Group } from '../../reakit-components/index'
+import { Button, Group, Tabs, TabsTab } from '../../reakit-components/index'
 import { Shadow } from 'reakit'
-import { MainContent } from './MainContent'
+import { indexOf, pick, pluck } from 'ramda'
+import { TaskTabsContainer } from './TaskTabsContainer'
+import { Keyed } from '../../shared-components/Keyed'
+import { TaskGroup } from './TaskGroup'
+import { Task } from './Task'
 
+export const MainContent = composeHOC()(function MainContent() {
+  const tabsList = store.getTabs()
+  const currentTabId = store.getCurrentTabId()
+  const tabIds = pluck('id')(tabsList)
+  return (
+    <TaskTabsContainer
+      initialState={{
+        ids: tabIds,
+        current: indexOf(currentTabId)(tabIds),
+      }}
+      setTabId={store.setTabId}
+    >
+      {tabProps => (
+        <Fragment>
+          <Tabs>
+            <Keyed
+              as={TabsTab}
+              getProps={({ id, title }) => ({
+                tab: id,
+                children: title,
+                ...tabProps,
+              })}
+              list={tabsList}
+            />
+          </Tabs>
+          <Tabs.Panel tab={currentTabId} {...tabProps}>
+            <Keyed
+              as={TaskGroup}
+              list={store.getTaskGroups()}
+              getProps={group => ({ group })}
+              taskComponent={Task}
+              taskProps={pick([
+                'isTaskSelected',
+                'isTaskHovered',
+                'mouseEnterTask',
+                'mouseLeaveTask',
+                'selectTask',
+                'toggleSelectedTaskDone',
+                'deleteSelectedTask',
+              ])(store)}
+            />
+          </Tabs.Panel>
+        </Fragment>
+      )}
+    </TaskTabsContainer>
+  )
+})
 export const TasksPage = composeHOC()(function Page({ store }) {
   return (
     <ViewportHeightContainer className="bg-light-gray">
