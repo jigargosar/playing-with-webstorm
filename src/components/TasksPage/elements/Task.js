c09dabe05915a9ea0772e06473e850dc8a44168c
Jigar Gosar <jigar.gosar@gmail.com>
2018-08-31 16:48:37 +0530

    WIP

--- src/components/TasksPage/elements/Task.js
+++ src/components/TasksPage/elements/Task.js
@@ -1,22 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Base, styled } from 'reakit'
+import { ifProp } from 'styled-tools'
 import { primaryDark, primaryLight } from '../../../reakit-components'
 
-const TaskStyled = styled(Base)``
+const TaskStyled = styled(Base)`
+  ${ifProp('hovered', { backgroundColor: primaryLight })};
+  ${ifProp('selected', { color: '#fff', backgroundColor: primaryDark })};
+`
 
 function Task({ selected, hovered, ...otherProps }) {
-  return (
-    <TaskStyled
-      className="mv2 pv2 br2"
-      {...(selected
-        ? { color: '#fff', backgroundColor: primaryDark }
-        : hovered
-          ? { backgroundColor: primaryLight }
-          : {})}
-      {...otherProps}
-    />
-  )
+  return <TaskStyled className="mv2 pv2 br2" {...otherProps} />
 }
 
 Task.propTypes = {
