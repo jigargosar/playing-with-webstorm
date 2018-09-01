6239318c7b623551f9cf4a63bcb7d3e2c4964598
Jigar Gosar <jigar.gosar@gmail.com>
2018-09-01 08:29:10 +0530

    WIP

--- src/components/TasksPage/TasksContainer.js
+++ src/components/TasksPage/TasksContainer.js
@@ -1,15 +1,51 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Container } from 'constate'
-import { createSampleTaskList } from '../../models'
+import {
+  createSampleTaskList,
+  flattenGroupTasks,
+  getTaskGroupsForTab,
+} from '../../models'
+import path from 'ramda/es/path'
+import { compose } from 'ramda'
+import { clampIdx } from '../../lib/ramda-strict'
 
 const initialState = {
   taskCollection: createSampleTaskList(),
   selectedTaskIdx: 0,
 }
 
-export const TasksContainer = ({ tabsProps, ...otherProps }) => (
-  <Container {...otherProps} initialState={initialState} />
+const taskCollectionFromState = path(['taskCollection'])
+const taskGroupsFromState = state =>
+  getTaskGroupsForTab(state.currentTabId, taskCollectionFromState(state))
+
+const currentTaskListFromState = compose(
+  flattenGroupTasks,
+  taskGroupsFromState,
+)
+const selectedTaskFromState = state => {
+  const selectedTaskIdx = clampIdx(state.selectedTaskIdx)(
+    currentTaskListFromState(state),
+  )
+  return compose(
+    path([selectedTaskIdx]),
+    currentTaskListFromState,
+  )(state)
+}
+const selectors = {
+  getTaskGroups: () => taskGroupsFromState,
+  getSelectedTask: () => selectedTaskFromState,
+}
+export const TasksContainer = props => (
+  <Container
+    {...props}
+    initialState={{
+      ...initialState,
+      currentTabId: props.tabProps.getCurrentId(),
+      tabProps: props.tabProps,
+    }}
+    selectors={{ ...selectors, ...props.selectors }}
+  />
 )
 
 TasksContainer.propTypes = {
